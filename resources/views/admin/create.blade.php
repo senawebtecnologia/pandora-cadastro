8e12fddc2af78fa41731e892cdf4f86330f448c0
Arão Domingos <araodomingos04@gmail.com>
2021-10-12 11:48:47 +0100

    adicionado alguns plugin e o formulario para cadastro de usuario

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -3,5 +3,54 @@
 @section('title', 'Criar um usuario')
 
 @section('content')
-    create
+    <div class="col-md-12 col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="header-title">Criar Um Usuario</h6>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('admin.user.store') }}">
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="userName">Nome</label>
+                            <input type="text" class="form-control" id="userName">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label>Data de Nascimento</label>
+                            <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                                <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate"/>
+                                <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                                    <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="userAddress">Endereço</label>
+                            <input type="text" class="form-control" id="userAddress">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="userNumber">Telefone</label>
+                            <input type="number" class="form-control" id="userNumber">
+                        </div>
+                        {{-- <div class="form-group col-md-6">
+                            <label for="userComplent">Complemento</label>
+                            <input type="text" class="form-control" id="userComplent">
+                        </div> --}}
+                        <div class="form-group col-md-6">
+                            <label for="userCity">Cidade</label>
+                            <input type="text" class="form-control" id="userCity">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="userCity">Estado</label>
+                            <input type="text" class="form-control" id="userCity">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-success" type="submit">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,22 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{ asset('assets/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/daterangepicker/daterangepicker.css') }}">
+  <!-- iCheck for checkboxes and radio inputs -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
+  <!-- Bootstrap Color Picker -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.min.css') }}">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/select2/css/select2.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+  <!-- Bootstrap4 Duallistbox -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css') }}">
+  <!-- BS Stepper -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/bs-stepper/css/bs-stepper.min.css') }}">
+  <!-- dropzonejs -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/dropzone/min/dropzone.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('assets/dist/css/adminlte.min.css') }}">
 </head>
@@ -140,13 +156,17 @@
 </div>
 <!-- ./wrapper -->
 
-<!-- jQuery -->
 <script src="{{ asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-<!-- Bootstrap 4 -->
 <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<!-- AdminLTE App -->
+<script src="{{ asset('assets/plugins/select2/js/select2.full.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/moment/moment.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/inputmask/jquery.inputmask.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{ asset('assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js')}}"></script>
+<script src="{{ asset('assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
 <script src="{{ asset('assets/dist/js/adminlte.min.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('assets/dist/js/init.js') }}"></script>
 <script src="{{ asset('assets/dist/js/demo.js') }}"></script>
+
 </body>
 </html>
