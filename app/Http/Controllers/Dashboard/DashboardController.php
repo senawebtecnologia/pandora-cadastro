b28155694906bde8db99fd6e0b1cbca129c6cbf3
Arão Domingos <araodomingos04@gmail.com>
2021-10-16 14:12:19 +0100

    verificando e mostrando o numero de usuarios na pagina inicial(dashboard)

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Dashboard;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Models\Contact;
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $users = Contact::all()->count();
+        return view('dashboard.index', compact('users'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,57 @@
 @extends('layouts.app')
 
-@section('tile', 'Dashboard')
+@section('title', 'Dashboard')
 
 @section('content')
-    
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-info">
+                <span class="info-box-icon"><i class="fas fa-users"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Usuarios</span>
+                    <span class="info-box-number">{{ $users }}</span>
+                    <div class="progress">
+                        <div class="progress-bar" style="width: {{ $users . '%' }}"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-success">
+                <span class="info-box-icon"><i class="far fa-thumbs-up"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Likes</span>
+                    <span class="info-box-number">41,410</span>
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 70%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-warning">
+                <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Events</span>
+                    <span class="info-box-number">41,410</span>
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 70%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6 col-12">
+            <div class="info-box bg-danger">
+                <span class="info-box-icon"><i class="fas fa-comments"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Comments</span>
+                    <span class="info-box-number">41,410</span>
+
+                    <div class="progress">
+                        <div class="progress-bar" style="width: 70%"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
