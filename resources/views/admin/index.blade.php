9cce51a706be565b7f76b3e58a788a1e32a7631c
Arão Domingos <araodomingos04@gmail.com>
2021-10-16 13:49:12 +0100

    estiliando a view ou pagina para exibir os dados de um usuario

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -30,7 +30,7 @@
                             <td class="text-center py-2">
                                 <div class="btn-group">
                                     <a href="{{ route('admin.user.show', ['user' => $user->id ]) }}" class="btn btn-primary"><i class="fas fa-eye"></i></a>
-                                    <a href="{{ route('admin.user.edit', ['user' => $user->id ]) }}" class="btn btn-info mr-1"><i class="fas fa-edit"></i></a>
+                                    <a href="{{ route('admin.user.edit', ['user' => $user->id ]) }}" class="btn btn-info mr-1"><i class="fas fa-pencil-alt"></i></a>
                                     <form action="{{ route('admin.user.delete', ['user' => $user->id ]) }}" method="post" class="row">
                                         @csrf
                                         @method('delete')
--- resources/views/admin/show.blade.php
+++ resources/views/admin/show.blade.php
@@ -3,19 +3,33 @@
 @section('title', 'Ver um usuario')
 
 @section('content')
-    
-<div class="card">
-    <div class="card-header">
-        <h6>Descrição do usuario</h6>
-    </div>
-    <div class="card-body">
-        <p>Nome</p>
-        <p>Nome</p>
-        <p>Nome</p>
-        <p>Nome</p>
-        <p>Nome</p>
-        <p>Nome</p>
 
-    </div>
+<div class="row">
+    <div class="col-md-9 col-lg-9" style="margin: 0 auto">
+        <div class="card">
+          <div class="card-header text-right text-muted">
+            Usuario
+          </div>
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="header text-center">
+                    <h2>{{ $user->primeiro_nome .' '. $user->segundo_nome }}</h2>
+                    <p class="small">{{ $user->data_nascimento }}</p>
+                </div>
+
+                <div class="content">
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li><b> Endereço: </b> {{ $user->endereco }}</li>
+                      <li><b> Cidade: </b> {{ $user->cidade }}</li>
+                      <li><b> Estado: </b> {{ $user->estado }}</li>
+                      <li><b> Complemento: </b> {{ $user->complemento }}</li>
+                    </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 </div>
 @endsection
